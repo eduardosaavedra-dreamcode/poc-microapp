355b3199a43e6b6251768a2aa39d3236493fdf4f
Eduardo Saavedra <eduardosaavedra@Eduardos-MacBook-Pro.local>
2021-02-15 11:40:05 -0500

    environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DevdacticLibModule } from 'devdactic-lib';
+import { DevMobilityLibModule } from 'dev-mobility-lib';
+import { environmentDevMobilityPoc } from 'src/environments/environmentDevMobilityPoc';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,8 +16,10 @@ import { DevdacticLibModule } from 'devdactic-lib';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
     DevdacticLibModule.forRoot({
       apiUrl: 'https://randomuser.me'
-    })],
+    }),
+    DevMobilityLibModule.forRoot(environmentDevMobilityPoc),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/environments/environmentDevMobilityPoc.ts
@@ -0,0 +1,9 @@
+export const environmentDevMobilityPoc = {
+    production: false,
+    logintr: 'https://dev.registro.solucionesbolivarsites.com/javascripts/basic-login-system-dev.js',
+    logintrenv: "devbolivarconmigo",
+    blogApiTerms: "https://4hrqwja612.execute-api.us-east-1.amazonaws.com/dev/bienestar/BlogAndTerms/getBienestarBlogApiTerms",
+    blogApiPolicies: "https://4hrqwja612.execute-api.us-east-1.amazonaws.com/dev/bienestar/BlogAndTerms/getBienestarBlogApiPolicies",
+    bienestarUrlApi: "https://4hrqwja612.execute-api.us-east-1.amazonaws.com/dev",
+    base64EncodedKeyFromJava: 'QUJDREVGR0hJSktMTU5PUA=='
+};
