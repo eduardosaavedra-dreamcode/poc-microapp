f0617e5c0593e533003119d6b01131a9bfbe6c78
Eduardo Saavedra <eduardosaavedra@Eduardos-MacBook-Pro.local>
2021-02-15 19:26:13 -0500

    menu

---
+++ devMobilityPoc/projects/dev-mobility-lib/src/lib/menu/menu.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { MenuComponent } from './menu.component';
+
+
+
+ 
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+  ],
+  declarations: [MenuComponent]
+})
+export class MenuModule {}
