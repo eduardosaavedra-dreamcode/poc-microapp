177b2cab6c5cc58b52c1ac5d728b9810c639d4d6
Eduardo Saavedra <eduardosaavedra@Eduardos-MacBook-Pro.local>
2021-02-19 23:36:29 -0500

    loading

--- src/app/provider/interceptor/interceptor.service.ts
+++ src/app/provider/interceptor/interceptor.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http'
 import { Observable, from } from 'rxjs';
-import { mergeMap, tap } from "rxjs/operators";
+import { finalize, mergeMap, tap } from "rxjs/operators";
 import { Storage } from '@ionic/storage';
-import { AlertController } from '@ionic/angular';
+import { AlertController, LoadingController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InterceptorService implements HttpInterceptor {
 
-  constructor(private storage: Storage, private alertCtrl: AlertController) { 
+  constructor(private storage: Storage, 
+    private alertCtrl: AlertController,
+    private loadinCtrl: LoadingController
+    ) { 
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.showLoading();
     let promise = this.storage.get('token');
     return from(promise).pipe(mergeMap(token => {
       let clonedReq = this.addToken(req, token);
       return next.handle(clonedReq).pipe(tap(
-        success => {success.type==0?console.log('Interceptado servicio de '+req.headers.get('Accept')):''},
+        success => {},
         async error => {
           if (error.status === 401) {
             // Unauthorized. You must ensure you receive a 401 error or something
@@ -27,9 +31,12 @@ export class InterceptorService implements HttpInterceptor {
           await this.showAlert(msg)
           return error;
         }
-      )
+      ),
+      finalize(()=>{
+        console.log('Interceptado servicio de '+req.headers.get('Accept'))
+        this.hideLoading();
+      })
       );
-
     }));
   }
 
@@ -59,6 +66,26 @@ export class InterceptorService implements HttpInterceptor {
       ]
     })
     await alertElement.present();
+  }
+
+  showLoading(){
+    this.loadinCtrl.getTop().then(hasLoading=>{
+      if(!hasLoading){
+        this.loadinCtrl.create({
+          spinner:'circular',
+          translucent: true
+        }).then(loading=> loading.present());
+      }
+    })
+  }
+
+  hideLoading(){
+    this.loadinCtrl.getTop().then(hasLoading=>{
+      if(hasLoading){
+        this.loadinCtrl.dismiss();          
+      }
+    })
+
   }
 
 }
