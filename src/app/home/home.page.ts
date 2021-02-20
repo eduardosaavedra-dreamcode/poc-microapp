acbe59a4305721202bcfdacd960825b2c76df7be
Eduardo Saavedra <eduardosaavedra@Eduardos-MacBook-Pro.local>
2021-02-20 11:38:21 -0500

    homne redux

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
+import { select, Store } from '@ngrx/store';
 import { DevdacticLibService } from 'devdactic-lib';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +11,20 @@ import { DevdacticLibService } from 'devdactic-lib';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  count$ : Observable<number>;
   childMessage = "";
   user = null;
   constructor(private devdacticLibService: DevdacticLibService,
     private router: Router,
-    private storage: Storage
+    private storage: Storage,
+    private store: Store<{ count: number}>
   ) {
    }
   ngOnInit(): void {
-    this.storage.set('appName', 'superApp');
+    this.storage.set('appName', 'superApp');    
+    this.count$ = this.store.pipe(
+      select('count')
+    );
   }
 
   getData() {
